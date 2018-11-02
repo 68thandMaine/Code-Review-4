b0179847c842456d1e3cf1089b0f7dcfa030073a
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-11-02 13:08:34 -0700

    allow size to determine cost when form is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 // Business logic for pizza
-function Pizza(size, meat, toppings) {
-  this.size = size //(sml, med, lg) shold be the values the ui collects
-  this.meat = meat //(should be determined by a funciton)
-  this.toppings = toppings//(should be determined by a funciton)
+function Pizza(size, toppings) {
+  this.size = size
+  this.toppings = 0 //(should be determined by a funciton)
 }
+
+
 
 Pizza.prototype.costBuilder = function() {
   var size;
@@ -14,12 +15,40 @@ Pizza.prototype.costBuilder = function() {
   } else if (this.size === "lg") {
     size = 14
   }
-var total = this.meat + this.toppings + size
-this.pizzaCost = total
+  var toppingCost = this.toppings
+  var total = toppingCost + size
+  this.pizzaCost = total
+  return this.pizzaCost.toFixed(2);
 }
 
 
-function toppingCost () {
-  var input = document.getElementsByName
 
+//////////////////
+//              //
+//   UI logic  //
+//             //
+/////////////////
+
+
+function addToppings() {
+  var input = document.getElementsByName("toppings")
+  var total = 0
+  for (var i = 0; i < input.length; i++) {
+    if (input[i].checked) {
+      total += parseFloat(input[i].value)
+    }
+  }
+  return total
 }
+
+$(document).ready(function() {
+  $("form#form1").submit(function(event) {
+    event.preventDefault();
+    var inputtedSize = $("#pizza-size").val();
+    var calculatedToppings = addToppings();
+    var newOrder = new Pizza(inputtedSize, calculatedToppings);
+    $(".total").text(newOrder.costBuilder());
+console.log(calculatedToppings);
+
+  })
+})
