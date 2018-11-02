e953246fd3f2cd14b4135f2776d0caa5b691005f
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-11-02 16:35:29 -0700

    add customer object and ability to disply entered information to order summary upon submit click

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,10 @@
-// Business logic for pizza
-function Pizza(firstName, lastName, address, phoneNumber, size, toppings) {
-  this.firstName = firstName,
-  this.lastName = lastName,
-  this.address = address,
-  this.phoneNumber = phoneNumber,
+///////////////////////////////
+// Business logic for pizza //
+/////////////////////////////
+function Pizza( size, toppings) {
   this.size = size,
   this.toppings = toppings //(should be determined by a funciton)
 }
-
-
-
 Pizza.prototype.costBuilder = function() {
   var size;
   if (this.size === "sml") {
@@ -25,6 +20,21 @@ Pizza.prototype.costBuilder = function() {
   return this.pizzaCost.toFixed(2);
 }
 
+
+//////////////////////////////////
+// Business logic for customer //
+/////////////////////////////////
+
+function Customer (firstName, lastName, address, phoneNumber){
+this.firstName = firstName,
+this.lastName = lastName,
+this.address = address,
+this.phoneNumber = phoneNumber
+}
+
+Customer.prototype.fullName = function () {
+  return this.firstName + " " + this.lastName
+}
 
 
  /////////////////
@@ -47,7 +57,12 @@ function addToppings() {
 function resetOrder () {
   $("#first-name").val("")
   $("#last-name").val("");
-  $("#address").val("");
+  $("#first-name").val("")
+  $("#last-name").val("")
+  $("#street").val("")
+  $("#city").val("")
+  $("#state").val("")
+  $("#zip-code").val("")
   $("#phone-number").val("");
   $("#pizza-size").val("");
   $('input[type=checkbox]').prop('checked',false);
@@ -58,12 +73,17 @@ $(document).ready(function() {
     event.preventDefault();
     var inputtedFirstName = $("#first-name").val();
     var inputtedLastName = $("#last-name").val();
-    var inputtedAddress = $("#address").val();
+    var inputtedAddress = (($("#street").val())+ " " +($("#city").val())+ " " +($("#state").val()) + " " + ($("#zip-code").val()))
+    console.log(inputtedAddress);
     var inputtedPhoneNumber = $("#phone-number").val();
     var inputtedSize = $("#pizza-size").val();
     var calculatedToppings = addToppings();
-    var newOrder = new Pizza(inputtedFirstName, inputtedLastName, inputtedAddress, inputtedPhoneNumber, inputtedSize, calculatedToppings);
-    $(".total").text(newOrder.costBuilder());
+    var newCustomer = new Customer(inputtedFirstName, inputtedLastName, inputtedAddress, inputtedPhoneNumber)
+    var newOrder = new Pizza( inputtedSize, calculatedToppings);
+    $("#total").text(newOrder.costBuilder());
+    $("#customer-name").text(newCustomer.fullName());
+    $("#customer-address").text(newCustomer.address)
+    $("#customer-phone-number").text(newCustomer.phoneNumber)
     resetOrder();
   })
 })
