75532c23522f3576b9ca1a4a2beea39ac28b66ae
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-11-02 09:18:17 -0700

    build prototype for determining the cost of a pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
-
 function Pizza(size, meat, toppings) {
-  this.size = size
-  this.meat = meat
-  this.toppings  = toppings
+  this.size = size //(sml, med, lg) shold be the values the ui collects
+  this.meat = 0 //(should be determined by a funciton)
+  this.toppings = 0//(should be determined by a funciton)
+}
+
+
+Pizza.prototype.costBuilder = function() {
+  var size;
+  if (this.size === "sml") {
+    size = 10;
+  } else if (this.size === "md") {
+    size = 13;
+  } else if (this.size === "lg") {
+    size = 14
+  }
+var total = this.meat + this.toppings + size
+this.pizzaCost = total
 }
